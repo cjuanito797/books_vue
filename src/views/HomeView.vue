1931aba85a5943ab6cd3ad8f9eb27b87f1008c56
Juan <jlizarraga@unomaha.edu>
2022-04-05 21:36:58 -0500

    commit all to branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="here is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to the Book Store
+        </p>
+        <p class="subtitle">
+          Home of the best books available.
+        </p>
+      </div>
+    </section>
+
+    <div class="column is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Books</h2>
+      </div>
+
+      <div class="column is-3" v-for="book in latestBooks"
+           v-bind:key="book.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="book.get_cover_art">
+          </figure>
+
+          <h3 class="is-size-4">{{book.name}}</h3>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios'
+
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      latestBooks: []
+    }
+  },
+
   components: {
-    HelloWorld
+  },
+
+  mounted() {
+    this.getLatestBooks()
+  },
+
+  methods: {
+    getLatestBooks() {
+      axios
+        .get('/api/v1/latest-books')
+        .then (response => {
+          this.latestBooks = response.data
+        })
+
+      .catch(error => {
+        console.log(error)
+      })
+
+    }
   }
 }
 </script>
